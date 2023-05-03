e86d4c935f1b1d9f03828f9b00ffb30297d297fb
LeviIver <levi.iverson@ndsu.edu>
2023-05-03 18:56:12 -0500

    get f and l name from db plus others
    get first name last name, make login work

--- ice-cream-shop/src/app/app.component.ts
+++ ice-cream-shop/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from './auth.service';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { account } from './models/account.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +12,29 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'ice-cream-shop';
-  user: any;
+  user$!: Observable<any>;
+  userid: any;
 
-  constructor(private afAuth: AngularFireAuth, private authService: AuthService) {} // Inject the AuthService
+  constructor(private afAuth: AngularFireAuth, private authService: AuthService, private db: AngularFireDatabase) { } // Inject the AuthService
 
   logout() {
     this.authService.signOut(); // Call signOut method on the instance of AuthService
+    window.location.reload()
   }
 
   ngOnInit() {
     this.afAuth.authState.subscribe((user) => {
-      this.user = user;
+      if (user != null) {
+        this.userid = user.uid;
+        console.log('test' + this.userid);
+
+        setTimeout(() => {
+          const account = this.db.object('/account/' + this.userid);
+          this.user$ = account.valueChanges();
+
+        }, 2000);
+      }
     });
+
   }
 }
