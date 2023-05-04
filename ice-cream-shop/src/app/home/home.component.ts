c2819c47d88d35e7fa30064bc8c11098f09bce6c
LeviIver <levi.iverson@ndsu.edu>
2023-05-03 19:36:12 -0500

    moved home message
    home message is now actually in the home component and no longer resides in the app component

--- ice-cream-shop/src/app/home/home.component.ts
+++ ice-cream-shop/src/app/home/home.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AuthService } from '../auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  title = 'ice-cream-shop';
+  user$!: Observable<any>;
+  userid: any;
+
+  constructor(private afAuth: AngularFireAuth, private authService: AuthService, private db: AngularFireDatabase) { } // Inject the AuthService
+ 
+
+  logout() {
+    this.authService.signOut(); // Call signOut method on the instance of AuthService
+    window.location.reload()
+  }
+
+  ngOnInit() {
+    this.afAuth.authState.subscribe((user) => {
+      if (user != null) {
+        this.userid = user.uid;
+        console.log('test' + this.userid);
+
+        setTimeout(() => {
+          const account = this.db.object('/account/' + this.userid);
+          this.user$ = account.valueChanges();
+
+        }, 2000);
+      }
+    });
+
+  }
+
+
 }
